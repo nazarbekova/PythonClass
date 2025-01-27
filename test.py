d3b5d7e090ae6354eeb832f9922d16f9e7974733
Jenishgul <jenishgul49@gmail.com>
2025-01-27 17:01:59 +0600

    calculator

--- test.py
+++ test.py
@@ -1,21 +1,34 @@
-
+def calculator():
    
     print("1. +")
     print("2. -")
     print("3. *")
     print("4. /")
     print("5. %")
-    print("6. **")
+    print("6. sqrt")
+    print("7. pow")
+    print("8. pow3")
 
-    oper ==imput("Operator: ")
-    
+    oper =input("hvjvhg")    
     num1 = int(input("Number 1: "))
     num2 = int(input("Number 2: "))
      
     if oper == '4':
         print(num1 /num2 )
-    elif oper == '5'
+    elif oper == '5':
         sum = (num1 * num2) /100
         print(sum)
+    elif choice =='6':
+        #import math
+        #print(math.sqrt(num1))
+        sqrt =num1 ** (0.5)
+        print(sqrt)  
+    elif choice =='7':
+        result = (num1 ** num2)
+        print(result)
+    elif choice =='8':
+       result = (num1**3)
+       print(result)
             
     
+calculator()
