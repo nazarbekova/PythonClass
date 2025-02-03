3e4cb7382318ceb34bc9f3a86da66db808e9cc66
Jenishgul <jenishgul49@gmail.com>
2025-02-03 16:07:57 +0600

    3

--- bmi.py
+++ bmi.py
@@ -7,10 +7,11 @@ if bmi < 18.5:
     print("underweight")
 elif 18.5 <= bmi <25:
     print("normalweight")
-elif 18.5 <= bmi <25:
+elif 25 <= bmi <30:
     print("normalweight")
-elif 
+elif 30 <= bmi <35:
     print("normalweight")
-elif 35 <= bim <39.5:
-    print("normalweight")
-
+elif 35 <= bmi <39.9:
+    print("odesity class 2")
+elif
+    print("super fat")
