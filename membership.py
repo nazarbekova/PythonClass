f8d295dc8741b930949a2b8f408ab3a1c17b5942
Jenishgul <jenishgul49@gmail.com>
2025-02-06 15:44:05 +0600

    оценка

--- membership.py
+++ membership.py
@@ -1,28 +1,47 @@
-member = int(input("1. Gold, 2. Silver, 3. normal"))
-Silver = float (input("amount: "))
-# Usual + float (input("Usual")) 
+# member = int(input("1. Gold, 2. Silver, 3. normal"))
+# Silver = float (input("amount: "))
+# # Usual + float (input("Usual")) 
 
-if member  == 1:
-    print("Above one hundred,")
-    amout = float(input("enter spending: "))
-    if amout > 100:
-        print(amout * .2)
-    else:
-        print(amout * 0.1)    
+# if member  == 1:
+#     print("Above one hundred,")
+#     amout = float(input("enter spending: "))
+#     if amout > 100:
+#         print(amout * .2)
+#     else:
+#         print(amout * 0.1)    
 
-if member  == 2:
-    print("and also above one hundred,")
-    amout = float(input("enter spending: "))
-    if amout > 100:
-        print( amout * .15)
-    else:
-        print(amout * 0.5)
+# if member  == 2:
+#     print("and also above one hundred,")
+#     amout = float(input("enter spending: "))
+#     if amout > 100:
+#         print( amout * .15)
+#     else:
+#         print(amout * 0.5)
 
-if member  == 3:
-    print("but not above one hundred,")
-    amout = float(input("enter spending: "))
-    if amout > 100:
-        print( amout * .05)
-    else:
-        print(amout * .0)
-      
+# if member  == 3:
+#     print("but not above one hundred,")
+#     amout = float(input("enter spending: "))
+#     if amout > 100:
+#         print( amout * .05)
+#     else:
+#         print(amout * .0)
+
+
+
+
+
+
+score = int(input("0-100"))
+
+if score > 90:
+    homework = input("вы сдали все Д.З (ДА/НЕТ):")
+    homework == "ДА"
+    print("Отлично! Оценка: +A")
+elif score < 90 and score > 80:
+    homework = input("вы сдали все Д.З (ДА/НЕТ):")
+    homework == "ДА"
+    print("Хорошо! Оценка: B")
+elif  score < 80 and score > 70:
+    homework = input("вы сдали все Д.З (ДА/НЕТ):")
+    homework == "ДА"
+    print("Удовлетворительно! Оценка: C")
